04ce0b38d3dae91a9a8b4e9319204d216a2bf716
ayveezub <ayveezub@gmail.com>
2023-11-02 19:13:25 +0300

    refactor solution to custom 'Parallel Letter Frequency' (js) exercise

--- custom/js/parallel-letter-frequency/index.js
+++ custom/js/parallel-letter-frequency/index.js
@@ -1,36 +1,37 @@
-const trim = text => text.toLowerCase().replaceAll(/^[0-9 _\\.]+$/g, "")
+const trimNonAlphabeticChars = text => text.toLowerCase().replaceAll(/^[0-9 _\\.]+$/g, "")
 
-const countLettersInText = text => {
+const countLetters = text => {
   const frequency = {}
 
   for (let char of text) {
-    if (char in frequency) {
-      frequency[char]++
-    } else {
-      frequency[char] = 1
-    }
+    if (char in frequency) frequency[char]++
+    if (char in frequency === false) frequency[char] = 1
   }
 
   return frequency
 }
 
-async function freq(texts) {
-  if (!texts) return {}
-
-  const frequencies = await Promise.all(texts.map(text => countLettersInText(trim(text))))
+const mergeResults = frequencies => {
   const totalFrequency = {}
 
   for (let frequency of frequencies) {
     for (let char in frequency) {
-      if (char in totalFrequency) {
-        totalFrequency[char] += frequency[char]
-      } else {
-        totalFrequency[char] = frequency[char]
-      }
+      if (char in totalFrequency) totalFrequency[char] += frequency[char]
+      if (char in totalFrequency === false) totalFrequency[char] = frequency[char]
     }
   }
 
   return totalFrequency
+}
+
+async function freq(texts) {
+  if (!texts) return {}
+
+  const frequencies = await Promise.all(
+    texts.map(text => countLetters(trimNonAlphabeticChars(text)))
+  )
+
+  return mergeResults(frequencies)
 }
 
 module.exports = freq
